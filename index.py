95ba350529b0b30f8a8dbbe38126d4327684aa8a
familoff <max.familoff@ya.ru>
2022-08-12 19:18:41 +0300

    Update index.py

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/index')
 def index():
     hello = "Получилось"
     return render_template('/templates/index.html', hello=hello)
