9a6bdeba27d5e21009c8957dde226f2e23a406ef
familoff <max.familoff@ya.ru>
2022-08-13 14:56:01 +0300

    js

--- index.py
+++ index.py
@@ -1,8 +1,12 @@
+import random
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-
-    return render_template('index.html')
+    cards = ["Пустыня", "Пустыня", "Пустыня", "Пустыня", "Пустыня", "Пустыня", "Пустыня", "Пустыня",
+             "Лес", "Лес", "Лес", "Лес", "Лес", "Лес", "Лес", "Гора", "Гора", "Гора", "Гора", "Гора", "Гора",
+             "Вода", "Вода", "Вода", "Вода", "Любой ландшафт", "Любой ландшафт",]
+    [cards.shuffle() for i in cards]
+    return render_template('index.html', cards=cards)
