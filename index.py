7d7056836c4fd4f3b7c0baea3be23031c83d0cc2
familoff <max.familoff@ya.ru>
2022-08-13 15:15:02 +0300

    python add

--- index.py
+++ index.py
@@ -1,21 +1,21 @@
 import random
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+play = 0
 cards = ["Пустыня", "Пустыня", "Пустыня", "Пустыня", "Пустыня", "Пустыня", "Пустыня", "Пустыня",
          "Лес", "Лес", "Лес", "Лес", "Лес", "Лес", "Лес", "Гора", "Гора", "Гора", "Гора", "Гора", "Гора",
          "Вода", "Вода", "Вода", "Вода", "Любой ландшафт", "Любой ландшафт", ]
 [random.shuffle(cards) for i in range(7)]
 def Round():
-    card = cards[0]
-
+    card1 = cards[0]
     cards.pop(0)
-    card = cards[0]
+    card2 = cards[0]
     cards.pop(0)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-
-
+    while play == (request.form['play']):
+        Round()
     return render_template('index.html', cards=cards)
