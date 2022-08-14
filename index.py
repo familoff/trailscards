f1c31633df50c86bd93c807dace737de435b1c0c
familoff <max.familoff@ya.ru>
2022-08-14 11:09:49 +0300

    python add

--- index.py
+++ index.py
@@ -1,21 +1,57 @@
 import random
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
-
-play = 0
+global cards, history
 cards = ["Пустыня", "Пустыня", "Пустыня", "Пустыня", "Пустыня", "Пустыня", "Пустыня", "Пустыня",
          "Лес", "Лес", "Лес", "Лес", "Лес", "Лес", "Лес", "Гора", "Гора", "Гора", "Гора", "Гора", "Гора",
          "Вода", "Вода", "Вода", "Вода", "Любой ландшафт", "Любой ландшафт", ]
 [random.shuffle(cards) for i in range(7)]
-def Round():
-    card1 = cards[0]
-    cards.pop(0)
-    card2 = cards[0]
-    cards.pop(0)
+play = 'Начать игру'
 
-@app.route('/', methods=['POST', 'GET'])
+cardimg = {'Пустыня': 'static/sand.png', 'Лес': 'static/wood.png', 'Гора': 'static/mount.png',
+           'Вода': 'static/water.png', 'Любой ландшафт': 'static/all.png', 'Back': 'static/back.png'}
+cardimg1 = ""
+cardimg2 = ""
+
+history = []
+history_count = 0
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    while play == (request.form['play']):
-        Round()
-    return render_template('index.html', cards=cards)
+    if len(cards) >= 26:
+        history.clear()
+    if request.method != 'POST':
+        play = 'Начать игру'
+        card1 = ""
+        card2 = ""
+        cardimg1 = cardimg.get("Back")
+        cardimg2 = cardimg.get("Back")
+    if request.method == 'POST':
+        play = 'Следующие карты'
+        card1 = cards[0]
+        card2 = cards[1]
+        cardimg1 = cardimg.get(card1)
+        cardimg2 = cardimg.get(card2)
+        history.append(cardimg1)
+        history.append(cardimg2)
+        global history_count
+        history_count +=1
+        cards.pop(0)
+        cards.pop(0)
+    if len(cards) <= 1:
+        play = 'Начать новую игру'
+        cardsadd()
+
+    return render_template('index.html', cards=cards, card1=card1, card2=card2, play=play, cardimg1=cardimg1, cardimg2=cardimg2, history=history, history_count=history_count)
+
+def cardsadd():
+    global cards
+    cards = ["Пустыня", "Пустыня", "Пустыня", "Пустыня", "Пустыня", "Пустыня", "Пустыня", "Пустыня",
+             "Лес", "Лес", "Лес", "Лес", "Лес", "Лес", "Лес", "Гора", "Гора", "Гора", "Гора", "Гора", "Гора",
+             "Вода", "Вода", "Вода", "Вода", "Любой ландшафт", "Любой ландшафт", ]
+    [random.shuffle(cards) for i in range(7)]
+    return cards
+
+if __name__ == '__main__':
+    app.run()
